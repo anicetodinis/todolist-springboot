320f32aebf683e79b9ed89d825e645156e9ed6e3
Aniceto Dinis Munguambe <87573026+anicetodinis@users.noreply.github.com>
2026-02-03 17:04:11 +0200

    feat: hash passowrd

--- src/main/java/mz/com/aniceto/todolist/user/UserController.java
+++ src/main/java/mz/com/aniceto/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package mz.com.aniceto.todolist.user;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /** 
  * Modificadores de acesso
@@ -43,7 +47,8 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Utilizador já existe");
         }
         var userCreated = this.userRepository.save(userModel);
-
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
