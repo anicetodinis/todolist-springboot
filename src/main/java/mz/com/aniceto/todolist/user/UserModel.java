3e52f10a9d3e1f13485abe2b62092d2e90c12c2d
Aniceto Dinis Munguambe <87573026+anicetodinis@users.noreply.github.com>
2026-02-03 15:05:02 +0200

    build: database config & users table created

--- src/main/java/mz/com/aniceto/todolist/user/UserModel.java
+++ src/main/java/mz/com/aniceto/todolist/user/UserModel.java
@@ -1,13 +1,27 @@
 package mz.com.aniceto.todolist.user;
 
+
+import java.time.LocalDate;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data  //Gerando automaticamente getters e setters atraves da dependencia lombok
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String username;
     private String name;
     private String password;
     private Integer age;
 
-    
+    @CreationTimestamp
+    private LocalDate createdAt;
 }
