667db49beee9d263fb0789f232aaa88abe012b28
Aniceto Dinis Munguambe <87573026+anicetodinis@users.noreply.github.com>
2026-02-03 16:22:14 +0200

    build: implement interface repository to users

--- src/main/java/mz/com/aniceto/todolist/user/UserController.java
+++ src/main/java/mz/com/aniceto/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package mz.com.aniceto.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
     
     /** 
      * Tipo de metodos
@@ -29,8 +33,8 @@ public class UserController {
      * etc
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println("Nome do utilizador: "+userModel.getName());
-
+    public UserModel create(@RequestBody UserModel userModel){
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
